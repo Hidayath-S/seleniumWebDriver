43eb741a4c4d08931694974a5846fe0108052f70
Hidayath-S <hiddu91@gmail.com>
2023-01-30 22:03:07 +0530

    [Framework]: Added method to capture screenshots for failed tests and added testNG Suite.

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,23 +1,31 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.SecureAreaPage;
 
+import java.io.File;
+import java.io.IOException;
+
 public class BaseTests {
 
-   private WebDriver driver;
-   protected HomePage homePage;
-   protected LoginPage loginPage;
-   protected SecureAreaPage secureAreaPage;
+    private WebDriver driver;
+    protected HomePage homePage;
+    protected LoginPage loginPage;
+    protected SecureAreaPage secureAreaPage;
 
     @BeforeClass
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
@@ -29,7 +37,23 @@ public class BaseTests {
     }
 
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
+    }
+
+
+    @AfterMethod
+    public void takeFailedTestsScreenShots(ITestResult iTestResult) {
+        try {
+            if (iTestResult.getStatus() == 2) {
+                TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+                File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+                Files.move(screenshot, new File("src/test/resources/screenshots/"+iTestResult.getMethod().getMethodName()+".png"));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
